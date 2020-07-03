4a9650543f64c52b819bfb2549348ee1b9302084
Eliam LOTONGA <e.m.lotonga@gmail.com>
2020-07-03 15:02:51 +0200

    modal post_image

--- app.js
+++ app.js
@@ -61,9 +61,15 @@ if(!window.location.origin.includes("127.0.0.1"))
 }
 
 // New post focus display new_post field as a modal
+let modal_bg = document.createElement("DIV");
+modal_bg.classList.add("modal_bg")
+
+let modal_img_container = document.createElement("DIV");
+let modal_img = document.createElement("IMG");
+
 let exit_modal_mode = document.querySelector(".card_head .account_options_links");
 exit_modal_mode.addEventListener("click", ()=>{
-    document.body.classList.remove("new_post_focused");
+    document.body.classList.remove("modal_open");
     let modal_bg = document.querySelector(".modal_bg");
     document.body.removeChild(modal_bg);
 });
@@ -71,14 +77,32 @@ let new_post_field = document.querySelector(".card.new_post_card #post.new_post"
 new_post_field.addEventListener("focus", () =>
 {
     //<div class="modal_bg"></div>
-    let modal_bg = document.createElement("DIV");
-    modal_bg.classList.add("modal_bg")
+    
 
-    modal_bg.addEventListener("click", ()=>{
-        document.body.classList.remove("new_post_focused");
-        document.body.removeChild(modal_bg);
-    })
-
-    document.body.classList.add("new_post_focused");
+    document.body.classList.add("modal_open");
     document.body.prepend(modal_bg)
-}); 
+});
+
+// Display post image in big version on click
+
+let post_images = document.querySelectorAll(".main_content .post_image");
+
+post_images.forEach(img=>{
+    img.addEventListener('click', ()=>{
+        modal_img_container.classList.add("modal_image_container")
+        modal_img.src = img.src;
+        modal_img.classList.add("modal_big_image")
+        modal_img_container.appendChild(modal_img);
+        modal_bg.appendChild(modal_img_container);
+        document.body.prepend(modal_bg);
+        document.body.classList.add("modal_open_img")
+    });
+});
+
+modal_bg.addEventListener("click", ()=>{
+    document.body.classList.remove("modal_open");
+    document.body.classList.remove("modal_open_img");
+    if(modal_bg.hasChildNodes())
+        modal_bg.removeChild(modal_img_container);
+    document.body.removeChild(modal_bg);
+})
